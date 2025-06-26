e802420879c7d60401c615fbbec5866621d96e58
AnhNguyen <hung000anh@gmail.com>
2025-06-26 16:44:44 +0700

    Refactor App component to integrate routing with MainLayout and Chart page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
 import MainLayout from './layouts/MainLayout'
+import { Routes, Route } from 'react-router-dom';
+import Chart from './pages/Chart'
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <MainLayout></MainLayout>
+      <Routes>
+        <Route path="/" element={<MainLayout />}>
+          <Route path="chart" element={<Chart />} />
+          {/* Thêm các route khác ở đây nếu cần */}
+        </Route>
+      </Routes>
+
     </>
   )
 }
