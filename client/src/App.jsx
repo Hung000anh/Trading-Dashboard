ef2978964adcdc53c8c521dae515b0c60a2050f8
AnhNguyen <hung000anh@gmail.com>
2025-07-06 12:48:15 +0700

    Add Home route to main application layout

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import MainLayout from './layouts/MainLayout'
 import { Routes, Route } from 'react-router-dom';
 import Chart from './pages/Chart'
 import Calendar from './pages/Calendar';
+import Home from './pages/Home';
 function App() {
   const [count, setCount] = useState(0)
 
@@ -10,6 +11,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<MainLayout />}>
+          <Route index element={<Home />} />
           <Route path="chart" element={<Chart />} />
           <Route path="calendar" element={<Calendar />} />
         </Route>
